19ef9893653e1cf0531c724e9c0c6a986237e52b
Iryna <kate_thebest@bigmir.net>
2014-01-10 01:21:44 +0400

    Using CRUD

--- app/models/User.java
+++ app/models/User.java
@@ -3,15 +3,23 @@ package models;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import play.data.validation.Email;
+import play.data.validation.Required;
 import play.db.jpa.Model;
 
 @Entity
 @Table(name = "blog_user")
 public class User extends Model {
 
+	@Email
+	@Required
 	public String email;
+	
+	@Required
 	public String password;
+	
 	public String fullName;
+	
 	public String isAdmin;
 
 	public User(String email, String password, String fullName) {
@@ -22,6 +30,10 @@ public class User extends Model {
 
 	public static User connect(String email, String password) {
 		return find("byEmailAndPassword", email, password).first();
+	}
+	
+	public String toString() {
+	    return email;
 	}
 
 }
