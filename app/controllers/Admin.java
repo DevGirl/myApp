337bb2f0b6a7dd6549be35cd9488a91b5b77a4e3
Iryna <kate_thebest@bigmir.net>
2014-01-11 00:20:24 +0400

    Guide 9

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -3,6 +3,7 @@ package controllers;
 import java.util.List;
 
 import models.Post;
+import models.Tag;
 import models.User;
 import play.mvc.Before;
 import play.mvc.Controller;
@@ -10,25 +11,76 @@ import play.mvc.With;
 
 @With(Security.class)
 public class Admin extends Controller {
-    
-    @Before
-    static void setConnecteUser(){
-        if(Security.isConnected()){
-            User user = User.find("byEmail", Security.connected()).first();
-            renderArgs.put("user", user.fullName);
-        }
-    }
-    
-    public static void index(){
-        String user = Security.connected();
-        List<Post> posts = Post.find("author.email", user).fetch();
-        render(posts);
-    }
 
-   public static void form(){
-        render();
-   }
+	@Before
+	static void setConnecteUser() {
+		if (Security.isConnected()) {
+			User user = User.find("byEmail", Security.connected()).first();
+			renderArgs.put("user", user.fullName);
+		}
+	}
 
-   public static void save(){
-   }
+	public static void index() {
+		String user = Security.connected();
+		List<Post> posts = Post.find("author.email", user).fetch();
+		render(posts);
+	}
+
+	public static void form() {
+		render();
+	}
+
+	public static void save(String title, String content, String tags) {
+		User author = User.find("byEmail", Security.connected()).first();
+		Post post = new Post(author, title, content);
+		for (String tag : tags.split("\\s+")) {
+			if (tag.length() > 0) {
+				post.tags.add(Tag.findOrCreateByName(tag));
+			}
+		}
+		validation.valid(post);
+		if(validation.hasErrors()){
+			render("@form", post);
+		}
+		post.save();
+		index();
+	}
+	
+	public static void form(Long id) {
+	    if(id != null) {
+	        Post post = Post.findById(id);
+	        render(post);
+	    }
+	    render();
+	}
+	
+	public static void save(Long id, String title, String content, String tags) {
+	    Post post;
+	    if(id == null) {
+	        // Create post
+	        User author = User.find("byEmail", Security.connected()).first();
+	        post = new Post(author, title, content);
+	    } else {
+	        // Retrieve post
+	        post = Post.findById(id);
+	        // Edit
+	        post.title = title;
+	        post.content = content;
+	        post.tags.clear();
+	    }
+	    // Set tags list
+	    for(String tag : tags.split("\\s+")) {
+	        if(tag.trim().length() > 0) {
+	            post.tags.add(Tag.findOrCreateByName(tag));
+	        }
+	    }
+	    // Validate
+	    validation.valid(post);
+	    if(validation.hasErrors()) {
+	        render("@form", post);
+	    }
+	    // Save
+	    post.save();
+	    index();
+	}
 }
